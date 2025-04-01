a81c1ccd1b4afad5660c59da2a6222a602e78c06
unknown <saptarshim44@gmail.com>
2025-04-01 11:51:11 +0530

    Adjacenct Matrix

---
+++ AdjacencyMatrix.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+void main(){
+    int vertices, edges, i, j,u,v;
+    printf("Enter the number of vertices: ");
+    scanf("%d", &vertices);
+    printf("Enter the number of edges: ");
+    scanf("%d", &edges);
+
+    int incidenceMatrix[vertices][vertices];
+
+    for(i=0;i<vertices;i++)
+        for ( j = 0; j < vertices; j++)
+            incidenceMatrix[i][j]=0;        
+
+    printf("Enter the edges\n");
+    for ( i = 0; i < edges; i++)
+    {
+        scanf("%d%d",&u,&v);
+        u--;
+        v--;
+        incidenceMatrix[u][v]=1;
+        incidenceMatrix[v][u]=1;
+    }
+
+
+    for ( i = 0; i < vertices; i++)
+    {
+        for ( j = 0; j < vertices; j++)
+        {
+            printf("%d",incidenceMatrix[i][j]);
+        }
+        printf("\n");
+        
+    }
+    
+    
+}
